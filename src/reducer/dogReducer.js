c3686594eecdd6a32e6b31e456bdbb540088d9ae
Leigh-Ann Crivella <la.crivella.2@gmail.com>
2019-09-03 10:09:06 -0700

    reducers

---
+++ src/reducer/dogReducer.js
@@ -0,0 +1,20 @@
+const initialState = {
+  dog: {
+    name: '',
+    breed: '',
+    age: 0
+  },
+  loading: false,
+  error: null
+};
+
+export default function reducer(state = initialState, action) {
+  switch(action.type) {
+    case FETCH_DOG_LOADING:
+      return { ...action, loading: true };
+    case FETCH_DOG:
+      return { ...state, loading: false, error: null, dog: action.payload };
+    default:
+      return state;
+  }
+}
